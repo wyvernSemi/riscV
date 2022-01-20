82ebc8fea80a6e3a4df424a5e1e2efc0e628014a
Simon Southwell <simon@anita-simulators.org.uk>
2022-01-20 09:28:03 +0000

    Added model of 16450 UART, registered with a base address of 0x80000000 in the cpurv32i.cpp top level. Can be programmed to generate interrupts. Transmitting a byte will display to the screen, and keyboard inputs are received and readable.

---
+++ iss/src/uart.h
@@ -0,0 +1,68 @@
+//=============================================================
+// 
+// Copyright (c) 2022 Simon Southwell. All rights reserved.
+//
+// Date: 20th January 2022
+//
+// Header for a model of a 16450 UART
+//
+// This file is part of the rv32_cpu ISS .
+//
+// This code is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The code is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ths code. If not, see <http://www.gnu.org/licenses/>.
+//
+//=============================================================
+
+#ifndef _UART_H_
+#define _UART_H_
+
+// -------------------------------------------------------------------------
+// INCLUDES
+// -------------------------------------------------------------------------
+
+
+// -------------------------------------------------------------------------
+// PUBLIC DEFINES
+// -------------------------------------------------------------------------
+
+// The  UART has 8 registers
+#define UART_NUM_REGS                   8
+#define UART_REG_ADDR_MASK              0xfffffffc
+
+// Up to 4 UART contexts supported
+#define MAX_NUM_UARTS                   4
+
+// Number of clock cycles for each bit (for timing modelling). Change to match system clock and baud rate.
+#define UART_TICKS_PER_BIT              1
+
+// -------------------------------------------------------------------------
+// PUBLIC TYPE DEFINITIONS
+// -------------------------------------------------------------------------
+
+typedef struct
+{
+    time_t               start_time[MAX_NUM_UARTS];
+    bool                 int_pending[MAX_NUM_UARTS];
+    uint32_t             uart_regs[MAX_NUM_UARTS][UART_NUM_REGS];
+} uart_state_t;
+
+// -------------------------------------------------------------------------
+// PUBLIC TYPE DEFINITIONS
+// -------------------------------------------------------------------------
+
+extern void              uart_write     (const uint32_t address, const uint32_t data, const int cntx = 0);
+extern void              uart_read      (const uint32_t address, uint32_t* data,      const int cntx = 0);
+extern bool              uart_tick      (const time_t   time,    bool &terminate,     const bool kbd_connected = false, const int cntx = 0);
+extern uart_state_t      get_uart_state (void);
+extern void              set_uart_state (const uart_state_t state);
+#endif
