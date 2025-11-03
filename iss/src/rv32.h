1fa5ccaceb83d73f6327a24f54950a94d4b86937
Simon Southwell <simon@anita-simulators.org.uk>
2025-11-03 10:18:57 +0000

    Updated ISS executables for new version

--- iss/src/rv32.h
+++ iss/src/rv32.h
@@ -38,8 +38,8 @@ class rv32 : public RV32_TARGET_INHERITANCE_CLASS
 public:
 
     static const int major_ver = 1;
-    static const int minor_ver = 2;
-    static const int patch_ver = 12;
+    static const int minor_ver = 3;
+    static const int patch_ver = 0;
 
     LIBRISCV32_API rv32(FILE* dbg_fp = stdout) : RV32_TARGET_INHERITANCE_CLASS(dbg_fp)
     {
